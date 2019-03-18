6c357189b90d9e7740a53db4ef18244ac1a6a6ae
zhangwenkai <69967696@qq.com>
2019-03-19 01:43:53 +0800

    '完成拆分Header静态组件'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import Vue from 'vue'
 import App from './App'
 
 import router from './router'
+import Header from './components/Header/Header'
+
+// 注册全局组件
+Vue.component('Header', Header)
 
 /* eslint-disable no-new */
 new Vue({
