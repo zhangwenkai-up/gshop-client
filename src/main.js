a097803795f2da78e5d17fee58236fa5142c671a
zhangwenkai <69967696@qq.com>
2019-03-20 02:13:14 +0800

    '拆分Star组件'

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import App from './App'
 import store from './store'
 import router from './router'
 import Header from './components/Header/Header'
+import Star from './components/Star/Star'
 
 // 注册全局组件
 Vue.component('Header', Header)
+Vue.component('Star', Star)
 
 /* eslint-disable no-new */
 new Vue({
